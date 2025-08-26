32d738f9eb4a199e49499874e0ac68bf450f7a35
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-08-26 12:04:42 +0530

    AS

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["Font_URL"],
+    allow_origins=[Font_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
